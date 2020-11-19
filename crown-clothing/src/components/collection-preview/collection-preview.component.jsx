6020f38f6a76b295dc31abbc35c4425e421c0ebc
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-11-19 20:57:40 +0200

    updating our app with collection items componenet and collection preview update styles

--- crown-clothing/src/components/collection-preview/collection-preview.component.jsx
+++ crown-clothing/src/components/collection-preview/collection-preview.component.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 import './collection-preview.styles.scss';
 
+import CollectionItem from '../../components/collection-items/collection-item.component';
+
 const CollectionPreview = ({ title, items }) => (
-  <div className="collectio-preview">
+  <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map((item) => (
-          <div key={item.id}>{item.name}</div>
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
   </div>
