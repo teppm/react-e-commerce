65fb0e9dcea18b1e6d79524fa224ef18d3e52df6
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-11-22 19:12:06 +0200

    app header created

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,11 +1,14 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
