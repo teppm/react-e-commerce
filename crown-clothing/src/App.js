75c2b2a4ca2ef0023f60a0fc8de757bf1a294599
Madis Tepp <54039418+teppm@users.noreply.github.com>
2020-12-04 12:42:53 +0200

    store user data in firebase

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -6,7 +6,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from '../src/firebase/firebase.utils';
+import { auth, creatUserProfileDocument } from '../src/firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,9 +20,8 @@ class App extends React.Component {
 
   componentDidMount() {
     // open subscription
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+      creatUserProfileDocument(user);
     });
   }
 
