d03ab95c920fac9c538040aa88f0fafb129fc60e
Madis Tepp <54039418+teppm@users.noreply.github.com>
2021-01-03 19:20:37 +0100

    ahoppingcart started

--- crown-clothing/src/components/header/header.component.jsx
+++ crown-clothing/src/components/header/header.component.jsx
@@ -7,6 +7,8 @@ import './header.styles.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+
 import { auth } from '../../firebase/firebase.utils';
 
 const Header = ({ currentUser }) => (
@@ -30,6 +32,7 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
