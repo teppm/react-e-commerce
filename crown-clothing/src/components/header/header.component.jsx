859bbce5b82cb5ec3e759e8cec30c593745a6809
Madis Tepp <54039418+teppm@users.noreply.github.com>
2021-01-03 20:10:40 +0100

    cart dropdown icon + cart reducer/action/types

--- crown-clothing/src/components/header/header.component.jsx
+++ crown-clothing/src/components/header/header.component.jsx
@@ -9,9 +9,11 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import CartIcon from '../cart-icon/cart-icon.component';
 
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import { auth } from '../../firebase/firebase.utils';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -34,11 +36,14 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    {/* if hidden is true render nothing, else render CartDropdown */}
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
