b62f4924cf50bb7230ab127e07840be1352eae27
Madis Tepp <54039418+teppm@users.noreply.github.com>
2021-01-23 18:28:25 +0100

    moved the logic to get collection from back end to be handled in shop.sagas.jsx

--- crown-clothing/src/redux/shop/shop.sagas.js
+++ crown-clothing/src/redux/shop/shop.sagas.js
@@ -1,9 +1,35 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
 
 import ShopActionTypes from './shop.types';
 
+import {
+    firestore,
+    convertCollectionSnapshopToMap,
+} from '../../firebase/firebase.utils';
+
+import {
+    fetchCollectionsSuccess,
+    fetchCollectionsFailure,
+} from './shop.actions';
+
 export function* fetchCollectionsAsync() {
-    yield console.log('Saga Has Fired!!!!!');
+    try {
+        const collectionRef = firestore.collection('collections');
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(convertCollectionSnapshopToMap, snapshot);
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message));
+    }
+
+    // THIS IS A THUNK VERSION OF ABOVE CODE
+    // collectionRef
+    //     .get()
+    //     .then((snapshot) => {
+    //         const collectionsMap = convertCollectionSnapshopToMap(snapshot);
+    //         dispatch(fetchCollectionsSuccess(collectionsMap));
+    //     })
+    //     .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
 }
 
 export function* fetchCollectionsStart() {
