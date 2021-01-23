9da0cf47b64ea0d7ff439ff2a9efea8b030021e3
Madis Tepp <54039418+teppm@users.noreply.github.com>
2021-01-23 17:48:40 +0100

    started redux-saga lesson. Installed saga and creat shop.sagas.jsx with initial funcitons

--- crown-clothing/src/pages/shop/shop.component.jsx
+++ crown-clothing/src/pages/shop/shop.component.jsx
@@ -6,12 +6,12 @@ import CollectionPageContainer from '../collection/collection.container';
 
 import { connect } from 'react-redux';
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
 class ShopPage extends React.Component {
   componentDidMount() {
-    const { fetchCollectionsStartAsync } = this.props;
-    fetchCollectionsStartAsync();
+    const { fetchCollectionsStart } = this.props;
+    fetchCollectionsStart();
   }
 
   render() {
@@ -34,7 +34,7 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
